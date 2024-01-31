298b2b8b12c28267aba4ac896e50b9f2a5695dd3
rravi3 <ravirahul660@gmail.com>
2024-01-30 16:17:30 -0800

    first commit

---
+++ precache-manifest.15620b006b6ed91285dec84e36bb49d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95455d8c5ec4fb7919e6cd37dbb187cc",
+    "url": "/social-media-app-mernstack/index.html"
+  },
+  {
+    "revision": "9b053ec82f7bfc45c2fb",
+    "url": "/social-media-app-mernstack/static/css/main.c64aa5c8.chunk.css"
+  },
+  {
+    "revision": "03e8b713034edd8a72d3",
+    "url": "/social-media-app-mernstack/static/js/2.8496a841.chunk.js"
+  },
+  {
+    "revision": "9c959e3da2a5c2e11e75e3e25dd7a76d",
+    "url": "/social-media-app-mernstack/static/js/2.8496a841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b053ec82f7bfc45c2fb",
+    "url": "/social-media-app-mernstack/static/js/main.f736dee3.chunk.js"
+  },
+  {
+    "revision": "42b0076416c3152bd23f",
+    "url": "/social-media-app-mernstack/static/js/runtime-main.4c1c818b.js"
+  },
+  {
+    "revision": "bafb374b1c03c10abf2068d559ce12c8",
+    "url": "/social-media-app-mernstack/static/media/memories.bafb374b.webp"
+  },
+  {
+    "revision": "ca80bc621688bbd24b196f815beb1d59",
+    "url": "/social-media-app-mernstack/static/media/notes-icon.ca80bc62.webp"
+  }
+]);
